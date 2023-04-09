3c8d1dfcb9f66dbcb7ac177d073e78656fe6bd55
Martin Podloucký <podlouckymartin@gmail.com>
2023-04-09 10:38:44 +0200

    Generalize response sending

--- src/index.ts
+++ src/index.ts
@@ -4,58 +4,57 @@ import type { RequestHandler, Request, Response } from "express";
 import type { Result } from 'monadix/result';
 import type { ZodType } from "zod";
 
-export interface ResultError<M extends Record<string, any> = Record<string, any>> {
+export interface EndpointError<M extends Record<string, any> = Record<string, any>> {
   status: number;
   code: string;
   detail: string;
   meta?: M;
 }
 
-interface ResouceDefinition<T> {
-  type: string;
-  validation?: ZodType<T>;
+type Resource<T> = {
+  id: string;
+} & T;
+
+type EndpointResult<T> = Resource<T> | Resource<T>[];
+
+interface EndpointDefinition<Body, Data> {
+  resourceType: string;
+  bodySchema?: ZodType<Body>;
   handler: (
     req: Request, res: Response,
-  ) => Result<T, ResultError[]> | Promise<Result<T, ResultError[]>>;
+  ) => (
+    | Result<EndpointResult<Data>, EndpointError[]>
+    | Promise<Result<EndpointResult<Data>, EndpointError[]>>
+  );
 }
 
 interface Jsonder {
   middleware: () => RequestHandler;
-  resource: <T>(definition: ResouceDefinition<T>) => RequestHandler;
+  endpoint: <Body, Data>(definition: EndpointDefinition<Body, Data>) => RequestHandler;
+  sendSuccess: (res: Response, result: EndpointResult<unknown>) => void;
+  sendFail: (res: Response, errors: EndpointError[]) => void;
 }
 
 interface JsonderOptions {
   serverUrl: string;
 }
 
-const sendFail = (res: Response, errors: ResultError[]) => {
-  const status = errors.length === 1
-    ? errors[0].status
-    : errors
-      .map((error) => Math.floor(error.status / 100))
-      .reduce((acc, cur) => (cur > acc ? cur : acc), 0) * 100;
-  
-  res.status(status).json({
-    status: 'fail',
-    errors,
-  });
-}
-
 const jsonder = (options: JsonderOptions): Jsonder => {
   return {
-    middleware: () => {
+    middleware() {
       const router = express.Router();
       router.use(express.json());
       router.use(cors());
       return router;
     },
-    resource: <T>(definition: ResouceDefinition<T>) => {
-      const { type, validation, handler } = definition;
+
+    endpoint<Body, Data>(definition: EndpointDefinition<Body, Data>) {
+      const { resourceType: type, bodySchema: validation, handler } = definition;
       
       return async (req: Request, res: Response) => {
         const validationResult = validation?.safeParse(req.body) ?? { success: true };
         if (!validationResult.success) {
-          sendFail(res, validationResult.error.issues.map(({ code, message, ...meta }) => ({
+          this.sendFail(res, validationResult.error.issues.map(({ code, message, ...meta }) => ({
             status: 400,
             code,
             detail: message,
@@ -67,14 +66,37 @@ const jsonder = (options: JsonderOptions): Jsonder => {
 
         const result = await handler(req, res);
         result
-          .map((data) => res.status(200).json({
-            status: 'success',
-            type,
-            url: `${options.serverUrl}${req.originalUrl}`,
-            result: data,
-          }))
-          .mapErr((errors) => sendFail(res, errors));
+          .map((eResult) => this.sendSuccess(res, eResult))
+          .mapErr((errors) => this.sendFail(res, errors));
         };
+    },
+
+    sendSuccess: (res: Response, result: EndpointResult<unknown>) => {
+      res.json({
+        status: 'success',
+        result: Array.isArray(result)
+          ? result.map((item) => ({
+            ...item,
+            url: `${options.serverUrl}${res.req.originalUrl}/${item.id}`,
+          }))
+          : {
+            ...result,
+            url: `${options.serverUrl}${res.req.originalUrl}`,
+          },
+      });
+    },
+
+    sendFail: (res: Response, errors: EndpointError[]) => {
+      const status = errors.length === 1
+        ? errors[0].status
+        : errors
+          .map((error) => Math.floor(error.status / 100))
+          .reduce((acc, cur) => (cur > acc ? cur : acc), 0) * 100;
+      
+      res.status(status).json({
+        status: 'fail',
+        errors,
+      });
     }
   }
 };
