c7b70b843de5d35b906aa90e8789c6ae796cbcb0
Martin Podloucký <podlouckymartin@gmail.com>
2023-04-12 11:03:54 +0200

    Allow for body, params and query validation

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import type { RequestHandler, Request, Response } from "express";
 import type { Result } from 'monadix/result';
-import type { ZodType } from "zod";
+import type { ZodIssue, ZodType } from "zod";
 
 export interface EndpointError<M extends Record<string, any> = Record<string, any>> {
   status: number;
@@ -17,9 +17,15 @@ type Resource<T> = {
 
 type EndpointResult<T> = Resource<T> | Resource<T>[];
 
-interface EndpointDefinition<Body, Data> {
+interface Validation {
+  bodySchema?: ZodType<unknown>;
+  querySchema?: ZodType<unknown>;
+  paramsSchema?: ZodType<unknown>;
+}
+
+interface EndpointDefinition<Data> {
   resourceType: string;
-  bodySchema?: ZodType<Body>;
+  validation?: Validation;
   handler: (
     req: Request, res: Response,
   ) => (
@@ -30,7 +36,7 @@ interface EndpointDefinition<Body, Data> {
 
 export interface Jsonder {
   middleware: () => RequestHandler;
-  endpoint: <Body, Data>(definition: EndpointDefinition<Body, Data>) => RequestHandler;
+  endpoint: <Data>(definition: EndpointDefinition<Data>) => RequestHandler;
   sendSuccess: (res: Response, result: EndpointResult<unknown>) => void;
   sendFail: (res: Response, error: EndpointError | EndpointError[]) => void;
 }
@@ -41,6 +47,19 @@ interface JsonderOptions {
   };
 }
 
+const issuesToErrors = (
+  issues: ZodIssue[],
+  location: 'body' | 'query' | 'params',
+): EndpointError[] => issues.map(({ code, message, ...meta }) => ({
+  status: 400,
+  code,
+  detail: message,
+  meta: {
+    location,
+    ...meta,
+  }
+}));
+
 const jsonder = (options?: JsonderOptions): Jsonder => {
   return {
     middleware() {
@@ -50,26 +69,37 @@ const jsonder = (options?: JsonderOptions): Jsonder => {
       return router;
     },
 
-    endpoint<Body, Data>(definition: EndpointDefinition<Body, Data>) {
-      const { resourceType: type, bodySchema: validation, handler } = definition;
+    endpoint<Data>(definition: EndpointDefinition<Data>) {
+      const { resourceType: type, validation, handler } = definition;
       
       return async (req: Request, res: Response) => {
-        const validationResult = validation?.safeParse(req.body) ?? { success: true };
-        if (!validationResult.success) {
-          this.sendFail(res, validationResult.error.issues.map(({ code, message, ...meta }) => ({
-            status: 400,
-            code,
-            detail: message,
-            meta,
-          })));
+        const bodyValidation = validation?.bodySchema?.safeParse(req.body) ?? { success: true };
+        const queryValidation = validation?.querySchema?.safeParse(req.query) ?? { success: true };
+        const paramsValidation = validation?.paramsSchema?.safeParse(req.params) ?? { success: true };
 
+        const errors = [];
+
+        if (!bodyValidation.success) {
+          errors.push(...issuesToErrors(bodyValidation.error.issues, 'body'));
+        }
+
+        if (!queryValidation.success) {
+          errors.push(...issuesToErrors(queryValidation.error.issues, 'query'));
+        }
+
+        if (!paramsValidation.success) {
+          errors.push(...issuesToErrors(paramsValidation.error.issues, 'params'));
+        }
+
+        if (errors.length > 0) {
+          this.sendFail(res, errors);
           return;
         }
 
         const result = await handler(req, res);
         result
           .map((eResult) => this.sendSuccess(res, eResult))
-          .mapErr((errors) => this.sendFail(res, errors));
+          .orElse((errors) => this.sendFail(res, errors));
         };
     },
 
